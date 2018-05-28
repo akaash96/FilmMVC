6cb8007029d579c5433c63b74c15a50bc1d3c5ae
akaash96 <akaash96@hotmail.co.uk>
2018-05-28 18:13:19 +0100

    Genre
    I have done the MVC for Genre, still some errors because there are still things I have to add in.

---
+++ FilmMVC/FilmMVC/Models/Genre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmMVC.Models
+{
+    public class Genre
+    {
+
+        public int GenreID { get; set; }
+
+        [Display(Name = "Genre Name")]
+        public string GenreName { get; set; }
+
+        // public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
