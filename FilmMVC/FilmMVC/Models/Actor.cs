19e59183aa7f352d99ad35ce6f97a9f59b38299e
akaash96 <akaash96@hotmail.co.uk>
2018-05-28 01:39:44 +0100

    Actor
    I have added in the MVC for actor

---
+++ FilmMVC/FilmMVC/Models/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FilmMVC.Models
+{
+    public class Actor
+    {
+
+        public int ActorID { get; set; }
+        public string ActorName { get; set; }
+        public int ActorCommentID { get; set; }
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
