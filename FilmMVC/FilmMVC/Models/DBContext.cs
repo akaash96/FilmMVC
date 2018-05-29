bd1ea5ce27859819355a681f351758626439133b
akaash96 <akaash96@hotmail.co.uk>
2018-05-29 18:14:16 +0100

    User/DBContext
    I have added the MVC for User and also DBContext. However, when I run it there are no links at the top. So i need to fix that.

---
+++ FilmMVC/FilmMVC/Models/DBContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FilmMVC.Models
+{
+    public class DBContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+        public DBContext() : base("name=DBContext")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<FilmMVC.Models.Movie> Movies { get; set; }
+
+        public System.Data.Entity.DbSet<FilmMVC.Models.Actor> Actors { get; set; }
+
+        public System.Data.Entity.DbSet<FilmMVC.Models.Director> Directors { get; set; }
+
+        public System.Data.Entity.DbSet<FilmMVC.Models.Genre> Genres { get; set; }
+
+        public System.Data.Entity.DbSet<FilmMVC.Models.User> Users { get; set; }
+
+        public System.Data.Entity.DbSet<FilmMVC.Models.MovieReview> MovieReviews { get; set; }
+
+        public System.Data.Entity.DbSet<FilmMVC.Models.ActorComment> ActorComments { get; set; }
+
+        public System.Data.Entity.DbSet<FilmMVC.Models.DirectorComment> DirectorComments { get; set; }
+
+    }
+}
